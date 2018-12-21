75b8436fb67a9c3a246a59f838e11319ca458cb4
shielsingh <sushilsingh4singh@gmail.com>
2018-12-21 16:49:40 +0530

    Checked In banckend

---
+++ quizbackend/Controllers/QuestionsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using quizbackend.Models;
+
+namespace quizbackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionsController : ControllerBase
+    {
+        // GET api/questions
+        [HttpGet]
+        public ActionResult<IEnumerable<Question>> Get()
+        {
+            return new Question[] {
+                new Question { Text = "Hello"},
+                new Question { Text = "Hi"}
+            };
+        }
+
+        // POST api/questions
+        [HttpPost]
+        public void Post([FromBody] Question question)
+        {
+
+        }
+    }
+}
